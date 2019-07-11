36e0485b7ed4e4fa89d29135fcee902a0ea1c12d
Kevin McKernan <kevin@mckernan.in>
2019-07-10 20:32:20 -0600

    configure siteconfig, redirect homepage to docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,27 +21,25 @@ const users = [
 ];
 
 const siteConfig = {
-  title: "Test Site", // Title for your website.
-  tagline: "A website for testing",
-  url: "https://your-docusaurus-test-site.com", // Your website URL
+  title: "ThunderED", // Title for your website.
+  tagline: "EVE Online Discord Bot",
+  url: "https://thundered.netlify.com", // Your website URL
   baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "test-site",
-  organizationName: "facebook",
+  projectName: "thundered",
+  organizationName: "panthernet",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: "home", label: "Docs Home" },
-    { doc: "overview", label: "Docs Overview" },
-    { page: "help", label: "Help" },
-    { blog: true, label: "Blog" }
+    { doc: "home", label: "Home" },
+    { page: "help", label: "Help" }
   ],
 
   // If you have users set above, you add it here:
@@ -73,7 +71,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} PantherNet`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
